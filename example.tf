502716f5f89f7c41251711b544ab7de267ae12dc
James <james.a0.lopez@gmail.com>
2020-12-21 14:55:44 -0600

    Adding amis variable.

--- example.tf
+++ example.tf
@@ -13,6 +13,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-08d70e59c07c61a3a"
+  ami           = var.amis[var.region]
   instance_type = "t2.micro"
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,10 @@
 variable "region" {
 }
+
+variable "amis" {
+	type = map(string)
+	default = {
+		"us-east-1" = "ami-b374d5a5"
+		"us-west-2" = "ami-fc0b939c"
+	}
+}
